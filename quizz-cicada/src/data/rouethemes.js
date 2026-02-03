b833255cdece0c4cf3f0bd353097aee02c2bb83a
zid04 <owonazida@gmail.com>
2026-02-03 18:49:11 +0100

    amelioration

---
+++ quizz-cicada/src/data/rouethemes.js
@@ -0,0 +1,16 @@
+export const Themes = [
+    "mot de passe",
+    "Malware ",
+    "Réseaux sociaux",
+    "Sécurité mobile",
+    "securite sur internet",
+    "securite de l'authentification",
+    "cyber attaques",
+    "monde du numérique",
+    "cyber espace",
+    "bonne pratiques",
+    "outils numeriques",
+    "reseaux",
+    "securite"
+
+];
